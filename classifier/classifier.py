79830a15f51cc5581d803a30c4f49760a774f8f8
keyangxu <xky0714@163.com>
2015-11-03 17:53:35 -0500

    classify

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -20,9 +20,12 @@ class Classifier:
     def __init__(self,train_file):
         self.train_file = train_file
         self.data = self.read_data(train_file)
+        self.read_lines(train_file)
+        self.X_S = self.data[0]
         self.X = self.data[0].toarray()
         self.y = self.data[1]
         self.n_fold = 10
+        assert len(self.lines)==(self.X.shape[0])
 
     '''
         read data from file
@@ -32,17 +35,17 @@ class Classifier:
         data = load_svmlight_file(train_file)
         return data
 
-    '''
-        preprocessing the input data
-    '''    
-    def preprocess(self):
-        idf = TfidfTransformer()
-        idf.fit_transform(self.X)
-        #self.X = preprocessing.normalize(self.X,axis=1)
+    def read_lines(self,train_file):
+        self.lines = open(train_file,"r").readlines()
 
     @staticmethod
     def sampling(X, y):
-        print 'test'
+        X_s = X
+        y_s = y
+        for i in range(len(y)):
+            if y[i] == 1:
+                X_s = numpy.vstack([X_s, X[i]])
+                y_s = numpy.vstack([y_s, y[i]])
 
     '''
         provide the classification of a single classifier
@@ -72,12 +75,22 @@ class Classifier:
         return [val_a/n_fold, val_p/n_fold, val_r/n_fold, val_f/n_fold]
 
 
+
+    '''
+        preprocessing the input data
+    '''    
+    def preprocess(self):
+        idf = TfidfTransformer()
+        idf.fit_transform(self.X)
+        #self.X = preprocessing.normalize(self.X,axis=1)
+
+
     '''
         This part provides the classification models.
     '''
-    def svm(self, kernel_f):
-        svc = SVC(kernel=kernel_f)
-        name  = 'Support Vector Machine'
+    def svm(self, c, kernel_f, g):
+        svc = SVC(C=c, kernel=kernel_f, gamma=g)
+        name  = 'Support Vector Machine with kernel: %s C:%d gamma:%f' %(kernel_f, c, g)
         res = self.classify(self.X, self.y, svc, self.n_fold)
         self.write_metrics(res, name)
 
@@ -103,10 +116,34 @@ class Classifier:
             metrics = ["accuracy", "precision", "recall", "f1-score"]
             for i in range(len(values)):
                 print 'Classifier %s has %s %.4f' %(name, metrics[i], values[i])
+            print '\n'
 
+    def split(self, n_fold):
+        cv = cross_validation.StratifiedKFold(self.y, n_folds=n_fold)
+        train = []
+        test = []
+        for train, test in cv:
+            break
+        write_file = open("train.train","w")
+        for itr in train:
+            write_file.write(self.lines[itr])
+        write_file = open("test.test","w")
+        for itr in test:
+            write_file.write(self.lines[itr])
+            
 if __name__ == '__main__':
-    c = Classifier('./train_file')
-    c.preprocess()
-    #c.svm("linear")
+    c = Classifier('./balanced.txt')
+    #c.preprocess()
+
+    c.split(5)
+
+    #crange = [100]
+    #grange = numpy.linspace(0, 0.10, num=20)
+    #for C in crange:
+    #    c.svm(C, 'linear', 0)
+    #for C in crange:
+    #    for g in grange:
+    #        c.svm(C, 'rbf', g)
+
     #c.gaussian_nb()
     #c.gradient_boosting()
