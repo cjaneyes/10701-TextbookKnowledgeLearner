5719ece5fc78eea447449c1f4ecd62d529b4f448
chenj <chenj@nlp.nju.edu.cn>
2015-11-02 21:59:31 -0500

    new classifier

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,9 +1,11 @@
 # !/usr/bin/python 
 
 import scipy
+import numpy
 from sklearn.datasets import load_svmlight_file
 from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import precision_score
 from sklearn.metrics import f1_score
@@ -11,15 +13,16 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.grid_search import ParameterGrid
+from sklearn.feature_extraction.text import TfidfTransformer
 
 class Classifier:
     # __init__
     def __init__(self,train_file):
         self.train_file = train_file
         self.data = self.read_data(train_file)
-        self.X = self.data[0]
+        self.X = self.data[0].toarray()
         self.y = self.data[1]
-        self.n_fold = 2
+        self.n_fold = 10
 
     '''
         read data from file
@@ -33,8 +36,13 @@ class Classifier:
         preprocessing the input data
     '''    
     def preprocess(self):
-        self.X = preprocessing.normalize(self.X,axis=1)
+        idf = TfidfTransformer()
+        idf.fit_transform(self.X)
+        #self.X = preprocessing.normalize(self.X,axis=1)
 
+    @staticmethod
+    def sampling(X, y):
+        print 'test'
 
     '''
         provide the classification of a single classifier
@@ -48,15 +56,13 @@ class Classifier:
         val_r = 0
         val_f = 0
         for train, test in cv:
-            clf.fit(X[train], y[train])
+            X_train = X[train]
+            y_train = y[train]
+
+            clf.fit(X_train, y_train)
             y_pred = clf.predict(X[test])
 
             # calculate evaluation metrics: [accuracy, precision, recall, f1_score]
-            #cnt = 0
-            #for i in range(len(y_pred)):
-            #    if y_pred[i] == y[test][i] and y_pred[i] == 1:
-            #        cnt += 1
-            #print cnt
 
             val_a += accuracy_score(y[test], y_pred)
             val_p += precision_score(y[test], y_pred, average='binary')
@@ -72,17 +78,22 @@ class Classifier:
     def svm(self, kernel_f):
         svc = SVC(kernel=kernel_f)
         name  = 'Support Vector Machine'
-        
-        res = self.classify(self.X.toarray(), self.y, svc, self.n_fold)
+        res = self.classify(self.X, self.y, svc, self.n_fold)
         self.write_metrics(res, name)
 
     def gaussian_nb(self):
         gnb = GaussianNB()
         name = 'Gaussian NB'
         
-        res = self.classify(self.X.toarray(), self.y, gnb, self.n_fold)
+        res = self.classify(self.X, self.y, gnb, self.n_fold)
         self.write_metrics(res, name)
+    
+    def gradient_boosting(self):
+        gbc = GradientBoostingClassifier()
+        name = 'GradientBoostingClassifier'
 
+        res = self.classify(self.X, self.y, gbc, self.n_fold)
+        self.write_metrics(res, name)
 
     '''
         Model part ends!
@@ -94,8 +105,8 @@ class Classifier:
                 print 'Classifier %s has %s %.4f' %(name, metrics[i], values[i])
 
 if __name__ == '__main__':
-    c = Classifier('./features.txt')
+    c = Classifier('./train_file')
     c.preprocess()
-    c.svm("rbf")
-    #c.GaussianLearn("all")
-
+    #c.svm("linear")
+    #c.gaussian_nb()
+    #c.gradient_boosting()
