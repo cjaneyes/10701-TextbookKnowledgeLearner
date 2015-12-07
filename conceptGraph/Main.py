9c1ec9b4182ec5f969e854d4f35d828588dc2e2f
keyangxu <xky0714@163.com>
2015-12-07 15:06:52 -0500

    annotation

--- conceptGraph/Main.py
+++ conceptGraph/Main.py
@@ -19,5 +19,5 @@ class Main:
 
 if __name__ == '__main__':
 	lexicon = Lexicon()
-	m = Main("./test/annotation.sub",lexicon)
+	m = Main("./test/demo.txt",lexicon)
 	m.output('./test/concepts_knowledges.txt')
--- conceptGraph/getSample.py
+++ conceptGraph/getSample.py
@@ -31,21 +31,31 @@ def function_filter(function, param1, param2):
 def binary_filter(predicate, param1, param2):
 	param1 = param1[0]
 	param2 = param2[0]
-	var_var_equals_filter = ['Equals', 'LessThan', 'GreaterThan','Congruent','Similar']
+	var_var_equals_filter = ['Equals', 'LessThan', 'GreaterThan','Congruent','Similar','IsHypotenuse','IsLeg']
 	if(predicate.name == 'AngleOf'):
 		if param1.name[:5] != 'angle':
 			return True
 		elif param2.name[:5] == 'angle':
 			return True
 	if predicate.name in var_var_equals_filter:
+		if param1.type == 0 and param1.name != 'Equals':
+			return True
 		if param1.type == 1 and param2.type == 1:
 			if param1.name[:len(param1.name)-1] != param2.name[:len(param2.name)-1]:
 				return True
 	return False
 
+def order_matter(predicate):
+	matter = ['LessThan', 'SmallerThan', 'GreaterThan']
+	if predicate.name in matter:
+		return True
+	if predicate.name[len(predicate.name)-2:] == 'Of':
+		return True
+	return False
+
 def getSample(sentence):
-	write_file_1 = open("uni.sub.literal","a")
-	write_file_2 = open("bi.sub.literal","a")
+	write_file_1 = open("uni.sup.literal","a")
+	write_file_2 = open("bi.sup.literal","a")
 	concept_list = reorder_concepts(sentence.concepts)
 	params = []
 	for concept in concept_list[0]:
@@ -72,6 +82,9 @@ def getSample(sentence):
 				for j in range(i+1,len(params)):
 					if not binary_filter(predicate, params[i], params[j]):
 						samples.append((predicate,[params[i],params[j]]))
+					if order_matter(predicate):
+						if not binary_filter(predicate, params[j], params[i]):
+							samples.append((predicate,[params[j],params[i]]))
 
 	for sample in samples:
 		print sentence.text
@@ -80,13 +93,13 @@ def getSample(sentence):
 			print sample[1][0][0].name+  "-" +str(sample[1][0][0].token_id) +")"
 			sam = Feature([sample[0],sample[1][0][0]], sentence)
 			features = sam.generateFeature()
-			write_file_1.write(sentence.text+"\n"+sample[0].name+"("+sample[1][0][0].name+  "-" +str(sample[1][0][0].token_id) +")\t"+str(features)+"\n")
+			write_file_1.write(sentence.text+"\n"+sample[0].name + "-" + str(sample[0].token_id) + "(" + sample[1][0][0].name+  "-" +str(sample[1][0][0].token_id) +")\t"+str(features)+"\n")
 
 		else:
 			print sample[1][0][0].name + "-" + str(sample[1][0][0].token_id) +","+sample[1][1][0].name + "-" + str(sample[1][1][0].token_id)+")"
 			sam = Feature([sample[0],sample[1][0][0],sample[1][1][0]], sentence)
 			features = sam.generateFeature()
-			write_file_2.write(sentence.text+"\n"+sample[0].name+"("+sample[1][0][0].name + "-" + str(sample[1][0][0].token_id) +","+sample[1][1][0].name + "-" + str(sample[1][1][0].token_id)+")\t"+str(features)+"\n")
+			write_file_2.write(sentence.text+"\n"+sample[0].name+ "-" + str(sample[0].token_id)+"("+sample[1][0][0].name + "-" + str(sample[1][0][0].token_id) +","+sample[1][1][0].name + "-" + str(sample[1][1][0].token_id)+")\t"+str(features)+"\n")
 
 		#print sample, features
 		
